d61b19abd0183b3bea520d734f86b1d3c4883a42
Jan Halík <halik.honza@gmail.com>
2024-06-18 17:30:10 +0200

    nyni pouziva TogglAPI

---
+++ main_new.py
@@ -0,0 +1,139 @@
+import csv
+import datetime
+import json
+import logging
+import os
+import urllib
+from dataclasses import dataclass
+from typing import Optional
+from uuid import uuid4
+import requests
+from ClockifyAPI import ClockifyAPI
+import base64
+
+formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+fileHandler = logging.FileHandler('log.txt')
+fileHandler.setFormatter(formatter)
+
+logger = logging.getLogger('clockify-automation')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+logger.addHandler(fileHandler)
+
+CSV_DATE_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+
+@dataclass
+class ServiceSettings:
+    token: str
+    workspace: str
+    email: Optional[str] = None
+
+
+def delete_entries(clockify: ClockifyAPI, clockify_settings: ServiceSettings, from_datetime: str):
+    clockify.deleteEntriesOfUser(
+        clockify_settings.email,
+        clockify_settings.workspace,
+        datetime.datetime.strptime(from_datetime, CSV_DATE_TIME_FORMAT).astimezone(datetime.timezone.utc)
+    )
+
+
+def main():
+    clockify_settings = ServiceSettings(
+        config['ClockifyApiKey'],
+        config['ClockifyWorkspace'],
+        config['ClockifyAdminEmail']
+    )
+
+    toggle_settings = ServiceSettings(
+        config['ToggleApiKey'],
+        config['ToggleWorkspace']
+    )
+
+    clockify = ClockifyAPI(clockify_settings.token, clockify_settings.email, reqTimeout=1)
+    clockify.getProjects(workspace=clockify_settings.workspace)
+
+    toggle_api_key = config['ToggleApiKey']
+    toggle_workspace = config['ToggleWorkspace']
+
+    toggle_base_url = 'https://api.track.toggl.com/api/v9'
+    toggle_reports_url = f'{toggle_base_url}/reports/api/v2'
+
+    email = config['ToggleFilterUser']
+    password = config['TogglePassword']
+    auth_string = f"{email}:{password}"
+    encoded_auth_string = base64.b64encode(auth_string.encode("ascii")).decode("ascii")
+
+    headers = {
+        'Authorization': f'Basic {encoded_auth_string}',
+        'Content-Type': 'application/json'
+    }
+    # get time entries
+    file_name = f'{uuid4()}.csv'
+    try:
+        report_response = requests.get(f'{toggle_base_url}/me/time_entries', headers=headers)
+        report_response.raise_for_status()
+        report_data = report_response.json()
+        print(len(report_data))
+        print(report_data)
+        with open(file_name, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=report_data[0].keys())
+            writer.writeheader()
+            writer.writerows(report_data)
+    except requests.exceptions.RequestException as e:
+        logger.error(f'Error while getting data from Toggl: {str(e)}')
+        return
+
+    if config.get('DeleteExistingFrom') is True and config.get('DryRun') is False:
+        delete_entries(clockify, clockify_settings, f'{start} 00:00:00')
+
+    with open(file_name, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            #TODO check if the row is valid according to filters for user and workspace in config.json
+            #if config.get('ToggleFilterClient') and config['ToggleFilterClient'] != row['Client']:
+            #    continue
+            #if config.get('ToggleFilterUser') and config['ToggleFilterUser'] != row['Email']:
+            #    continue
+            if row["stop"] == "":
+                continue
+            
+            logger.info(row)
+
+            start = datetime.datetime.strptime(row["start"], "%Y-%m-%dT%H:%M:%S%z").strftime(CSV_DATE_TIME_FORMAT)
+            start = datetime.datetime.strptime(start, CSV_DATE_TIME_FORMAT)
+            
+            end = datetime.datetime.strptime(row["stop"], "%Y-%m-%dT%H:%M:%S%z").strftime(CSV_DATE_TIME_FORMAT)
+            end = datetime.datetime.strptime(end, CSV_DATE_TIME_FORMAT)
+
+            tags = [tag.strip() for tag in row['tags'].split(',') if tag.strip() != '']
+
+            # tag billable if there's a tag billable
+            billable = 'billable' in tags
+            # remove billable and non-billable tags as we don't need them anymore
+            tags = [tag for tag in tags if tag not in {'non-billable', 'billable'}]
+
+            if config.get('DryRun') is False:
+                clockify.addEntry(
+                    start=start,
+                    description=row['description'],
+                    projectName= "internal", #row['project'], #TODO find where to get project name, i only have project id from csv output
+                    userMail=clockify_settings.email,
+                    workspace=clockify_settings.workspace,
+                    end=end,
+                    billable=billable
+                )
+            else:
+                logger.info('Dry run - nothing is sent to Clockify.')
+
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+if __name__ == '__main__':
+    main()
